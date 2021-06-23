bb4f70e81442a9302f4b364b431affc8e668a34b
Marlon Ourives <marloncicero_araujo@hotmail.com>
2021-06-23 10:19:02 -0400

    Fluxo de entrar em uma sala

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,15 +1,18 @@
 import { useHistory } from "react-router-dom";
-import { auth, firebase } from "../services/firebase";
+import { FormEvent } from "react";
+import { auth, database, firebase } from "../services/firebase";
 import illustrationImg from "../assets/images/illustration.svg";
 import logoImg from "../assets/images/logo.svg";
 import googleIconImg from "../assets/images/google-icon.svg";
 import "../styles/auth.scss";
 import { Button } from "../components/Button";
 import { useAuth } from "../hooks/useAuth";
+import { useState } from "react";
 
 export function Home() {
   const history = useHistory();
   const { user, signInWithGoogle } = useAuth();
+  const[ roomCode, setRoomCode] = useState('');
 
   async function handleCreateRoom() {
     if (!user) {
@@ -17,6 +20,23 @@ export function Home() {
     }
     history.push("/rooms/new");
   }
+
+  async function handleJoinRoom(event: FormEvent) {
+    event?.preventDefault();
+
+    if(roomCode.trim() === ''){
+      return ; 
+    }
+    const roomRef = await database.ref(`rooms/${roomCode}`).get();
+
+    if(!roomRef.exists()){ 
+      alert('Room does not exists.')
+      return ;
+    }
+
+    history.push(`/rooms/${roomCode}`)
+  }
+
 
   return (
     <div id="page-auth">
@@ -36,8 +56,13 @@ export function Home() {
             Crie sua sala com o Google
           </button>
           <div className="separator">ou entre em uma sala</div>
-          <form>
-            <input type="text" placeholder="Digite o código da sala" />
+          <form onSubmit={handleJoinRoom}>
+            <input 
+            type="text" 
+            placeholder="Digite o código da sala" 
+            onChange={event => setRoomCode(event.target.value)}
+            value={roomCode}
+            />
             <Button type="submit">Entrar na sala</Button>
           </form>
         </div>
